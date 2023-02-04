1fc0b93d3e1607d542a9da80e4da2984768ac173
Paula Schmidel <120142193+PaulaSchmidel@users.noreply.github.com>
2023-02-04 10:47:40 -0300

    Update M1S01E1_imprimaDescobertaDoBrasil.java

--- M1S01E1_imprimaDescobertaDoBrasil.java
+++ M1S01E1_imprimaDescobertaDoBrasil.java
@@ -1,13 +1,14 @@
 package semana1;
-import java_scanner;
 
-public  class  Ex01_imprimaDescobertaBrasil {
+import java.util.Scanner;
+
+public  class  Ex01_impimaDescobertaBrasil {
 public static void main (String [] args){
 
-    Scanner ler = new Scanner (system.in);
+    Scanner ler = new Scanner (System.in);
 
     /*[M1S01] Ex 1 - Imprima descoberta do Brasil
-    Crie um programa que receberá nas variáveis ​​dia, mes, ano e pais os respectivos valores
+    Crie um programa que receberá nas variáveis dia, mes, ano e pais os respectivos valores
     22 de abril de 1500 e Brasil. O programa deve exibir a seguinte mensagem:
     “O Brasil foi descoberto no dia 22 de abril de 1500!”, usando os valores das variáveis.
     */
@@ -17,8 +18,8 @@ public static void main (String [] args){
     int  ano = 1500 ;
     String  pais = "Brasil" ;
 
- System.out.println ( "Quando o Brasil foi descoberto?\n" + "O " + pais+ " foi descoberto no dia " + dia
-        + "de" + mes + "de" + ano );
+ System.out.println ( "Quando o Brasil foi descoberto?\n" + "O " + pais + " foi descoberto no dia " + dia
+        + " de " + mes + " de " + ano + ".");
 
     }
 }
